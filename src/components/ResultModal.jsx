8b08ad04b3a4bfda60deb66995efbd488f960a40
amiro hatami <amirohatami@amiros-Laptop.local>
2024-10-21 12:27:43 +0330

    adding createPortal from react-dom and telleport dialog pop up from down div section in timerChallnege to the root of the page near to top , its better for styling purposes and ...

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
   { remainingTime, targetTime , onReset },
@@ -17,8 +18,9 @@ const ResultModal = forwardRef(function ResultModal(
       },
     };
   });
-  return (
-    <dialog ref={dialog} className="result-modal">
+  return createPortal(
+// You can close the dialog with ESC button on keyboard with the help of onClose prop, I bind it with onRest :D 
+    <dialog ref={dialog} className="result-modal" onClose={onReset}> 
       {userLost && <h2>You Lost</h2>}
       {!userLost && <h2>Your Score:{score}</h2>}
       <p>
@@ -31,7 +33,7 @@ const ResultModal = forwardRef(function ResultModal(
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button> 
       </form>
-    </dialog>
+    </dialog>, document.getElementById('modal')
   );
 });
 export default ResultModal;
